34040da46cac691fb1c117eae51058f6d88fb683
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-12 15:33:17 -0700

    add script 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 	printf("Size of char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of int: %d byte(s)\n", (int) sizeof(int));
-	printf("Size of long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("Size of float: %d byte(s)\n", (int) sizeof(float));
-		return (0);
+		printf("Size of int: %d byte(s)\n", (int) sizeof(int));
+			printf("Size of long int: %d byte(s)\n", (int) sizeof(long));
+				printf("Size of long long int: %d byte(s)\n", (int) sizeof(long long));
+					printf("Size of float: %d byte(s)\n", (int) sizeof(float));
+						return (0);
 	
 }
