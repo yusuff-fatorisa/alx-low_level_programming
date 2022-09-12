9f2e35d643ced14cf5d55e186d351e00ce3fe43f
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-12 15:28:49 -0700

    add script 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ * Description: prints the size of variou types on the computer it is compiled
+ * and run on
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", (int) sizeof(float));
+		return (0);
+	
+}
