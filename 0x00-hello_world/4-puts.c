8a8747ed08f38dfb1d5ad8903aaa548bc2c065f5
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-12 15:07:34 -0700

    add script 5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
  * Return: 0 after printing the function
  * /
  
-nt main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
