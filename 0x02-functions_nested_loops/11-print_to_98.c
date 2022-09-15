d678186455e02fad6d63731e88f23ffdfe084916
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-14 19:40:46 -0700

    add script 11-print_to_988.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,11 +5,15 @@
  *print_to_98 - prints all natural numbers from n to 98,
  *followed by new line
  *
+ * @n: print from this number
+ *
  */
 
 
 void print_to_98(int n)
 {
+	int i, j;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
