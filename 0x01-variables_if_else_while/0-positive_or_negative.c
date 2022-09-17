34c96ffc4892bada1667219518626cbfc3a7dd1c
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-17 16:35:44 -0700

    fix bug in script 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,10 @@
 #include <stdio.h>
 
 /**
- * main - print if the number is positive, zero or negative
+ * main - assigns a random number to int n everytime 
+ * it executes  and prints it
  *
- * Description: using the main  function
- * this program prints "Programming is positive, zero or negative"
- *
- * Return (0)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -17,21 +15,21 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes here * /
+
 
 	if (n > 0) 
 	{
-		printf("%i is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 
 	else if (n == 0)
 	{
-		printf("%i is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 
 	else if (n < 0)
 	{
-		printf("%i is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
