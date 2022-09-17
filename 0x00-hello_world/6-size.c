2048381fbad7e6a0fb1e9afb29742eeea6a812c9
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-17 15:38:10 -0700

    fix bug in script 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,19 @@
 
 /**
  * main - print the size of various types
- * Description: prints the size of variou types on the computer it is compiled
+ * Description: prints the size of various types
+ * on the computer it is compiled
  * and run on
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of char: %d byte(s)\n", (int) sizeof(char));
-		printf("Size of int: %d byte(s)\n", (int) sizeof(int));
-			printf("Size of long int: %d byte(s)\n", (int) sizeof(long));
-				printf("Size of long long int: %d byte(s)\n", (int) sizeof(long long));
-					printf("Size of float: %d byte(s)\n", (int) sizeof(float));
-						return (0);
-	
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+	return (0);
 }
