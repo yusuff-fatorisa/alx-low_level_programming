8648711d7644c499841eb285e78fc6b499b97ad4
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-18 05:48:21 -0700

    add script 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,20 +2,21 @@
 
 
 /**
- * main - prints lower case alphabets in reverse
+ * main - prints all the number of base 16 in lower case,
  * followed by a new line
  *
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
+	int n;
 	char ch;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
-
 	putchar('\n');
 	return (0);
 }
