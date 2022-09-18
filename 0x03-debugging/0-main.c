01b97290ac70b2cd475c1a3f670f107a5602ad77
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-18 15:07:00 -0700

    fix bug in script 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
