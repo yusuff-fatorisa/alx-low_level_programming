09580a80a92ddbaa4da17ae4f468d3df648c99ff
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-19 12:32:12 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * that a pointer is pointed to to 98
+ *
+ * @n: pointer to the variable to be updated
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This directory contains all tasks and projects related to
+    -- Pointers
+    -- Arrays
+    -- Strings
+as related to C-Programming language
