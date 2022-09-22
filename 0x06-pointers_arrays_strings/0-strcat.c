326e0ab6ca012428d68fbd0be676b30223733d39
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-22 08:13:11 -0700

    fix compile bug in script 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,6 +6,8 @@
  * @dest: string to append to
  *
  * @src: string to add
+ *
+ * Return: a pointer to the resulting string
  */
 
 char *_strcat(char *dest, char *src)
@@ -15,16 +17,16 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	while (dest[i] != '0')
+	while (dest[i] != '\0')
 		i++;
 
-	while(src[j] != '0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
-	dest[i] = '0';
+	dest[i] = '\0';
 
 	return(dest);
 }
