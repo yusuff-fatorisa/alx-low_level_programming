d2b37d10f5aa9e0986e3dc27938904f112ce691c
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-22 18:32:16 -0700

    add script 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,10 +8,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
-
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to use p
+	 * - only one statement
+	 * - you are not allowed to code anything else except this line of code
+	 */
 	*(p + 5) = 98;
-
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
