9dc66145d7c11aaac949b7f223891ebb3f7df843
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-26 14:57:16 -0700

    fix compile bug in script 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,7 +9,7 @@
  * Reutrn: pointer to the memory area s
  */
 
-char *_memset(char *s, *char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 	for (i = 0; i < n; i++)
