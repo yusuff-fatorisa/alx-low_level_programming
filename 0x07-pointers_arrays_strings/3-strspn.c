9d631692eb8bb2d42a620c8b09118644c7302ad3
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-26 19:15:00 -0700

    fix compile bug in script 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strspn - gets the length of a prefix substring
+ * _strspn - gets the length of a prefix substring
  * @s: the string to evaluate
  * @accept: the string containing the list of characters to match in s
  *
@@ -18,7 +18,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
-		for (j = 0; accept[j] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
@@ -27,7 +27,9 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 		if (flag == 0)
+		{
 			return (f);
+		}
 	}
 
 	return (0);
