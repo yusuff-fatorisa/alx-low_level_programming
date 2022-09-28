0c14fb01e2a1d342660dbad7344b8c44d0de26c0
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-27 20:18:56 -0700

    fix betty bug in script 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,6 +2,7 @@
 
 /**
  * _memset - fills memory with a constant byte
+ *
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
@@ -12,6 +13,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
