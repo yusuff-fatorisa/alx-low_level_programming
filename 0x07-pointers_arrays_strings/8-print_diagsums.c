ec20320d95864edb6b35e07ecf99dfc723a9b1ae
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-27 21:31:33 -0700

    fix compile error in script 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,7 +6,6 @@
  * of a square matrix of integers
  *
  * @a: the square matrix of which to print the sum of its diagonal
- *
  * @size: the size of the matrix
  */
 
@@ -21,8 +20,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum = sum + a[(size * i) + i];
-		sum1 = sum1 + a[(size * (i + 1)) - (i + 1)];
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
 	}
 
 	print("%d, %d\n", sum, sum1);
