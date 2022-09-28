b065a37894fd6148b87e041dbf920183bd624b24
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-27 20:47:42 -0700

    fix betty bug in script 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,6 +3,7 @@
 
 /**
  * *_strstr - locates a substring
+ *
  * @haystack: the string to search in
  * @needle: the substring to look for
  *
@@ -16,7 +17,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++ )
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
 				break;
