9b90d0ba96e05a2b35e36ccaca016fcbd607a521
yusuff-fatorisa <bosun4real009@gmail.com>
2022-09-30 13:52:59 -0700

    add script 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints out all the arguments it receives
+ * 
+ * @argc: the number of arguments received
+ * @argv: an array of all arguments received
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
