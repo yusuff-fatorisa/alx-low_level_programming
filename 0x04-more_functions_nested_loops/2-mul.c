1cbda96f5c2699cc4a4642e82c127969d6efcec4
yusuff-fatorisa <bosun4real009@gmail.com>
2022-10-01 15:12:02 -0700

    add script 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - mulitiplies two integers
+ * @a: the first integer parameter to be multiplied by b
+ * @b: the second integer parameter to be multiplied by a
+ *
+ * Return: the output of the operation
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
