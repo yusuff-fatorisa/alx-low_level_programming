b761b42b65573bddf2b5eb953e229de1dde26634
yusuff-fatorisa <bosun4real009@gmail.com>
2022-10-01 16:32:47 -0700

    add script 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line on the terminal
+ * @n: the number of times the _ character should be printed
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i < n; i++)
+			_putchar('-');
+		_putchar("\n");
+	}
+}
