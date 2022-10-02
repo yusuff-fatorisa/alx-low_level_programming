76540ab5019c95204b7d93adc4866afe8f1ee3d5
yusuff-fatorisa <bosun4real009@gmail.com>
2022-10-01 18:08:09 -0700

    add script 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers 1 - 100, followed by a new line 
+ * but prints "fizz" for multiples of 3's
+ * and prints "buzz" for multiples of 5's
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		} if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf("Buzz");
+		} if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
