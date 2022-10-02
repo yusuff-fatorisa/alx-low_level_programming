901f6d1c08075e44db38e380eb58e629c5e42daf
yusuff-fatorisa <bosun4real009@gmail.com>
2022-10-01 18:18:34 -0700

    fix output error in script 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,10 +18,10 @@ int main(void)
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
-		} if (i % 3 != 0 && i % 5 == 0)
+		} else if (i % 3 != 0 && i % 5 == 0)
 		{
 			printf("Buzz");
-		} if (i % 3 == 0 && i % 5 == 0)
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		} else if (i == 1)
