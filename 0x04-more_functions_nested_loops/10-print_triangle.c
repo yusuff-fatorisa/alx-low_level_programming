308a02b95b2aafd18742c9132c1c5dd8ac85a6dd
yusuff-fatorisa <bosun4real009@gmail.com>
2022-10-02 17:20:50 -0700

    fix betty error in script 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 
 /**
  * print_triangle - draws a triangle using multiple terminal lines and #'s
- * @size: the height of the triangle 
+ * @size: the height of the triangle
  */
 
 void print_triangle(int size)
