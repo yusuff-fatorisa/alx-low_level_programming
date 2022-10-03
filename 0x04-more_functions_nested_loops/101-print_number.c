689cb7bd48f911652e7ff070c64cdfe92df57498
yusuff-fatorisa <bosun4real009@gmail.com>
2022-10-02 18:13:28 -0700

    fix output error in script 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,40 +7,21 @@
 
 void print_number(int n)
 {
-	int i;
-	int d = 1;
-	unsigned int x = n;
-	unsigned int y = n;
-	int c = 0;
+	unsigned int n1;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n += 1;
-		n = -n;
-		y = n;
-		x = n;
-		x += 1;
-		y += 1;
+	} else
+	{
+		n1 = n;
 	}
 
-	while (x != 0)
+	if (n1 / 10)
 	{
-		x /= 10;
-		c++;
+		print_number(n1 / 10);
 	}
-	for (i = 1; i < c; i++)
-	{
-		d *= 10;
-	}
-	for (i = 0; i < c; i++)
-	{
-		_putchar(y / d + '0');
-		y = y % d;
-		d /= d;
-	}
+
+	_putchar((n1 % 10) + '0');
 }
