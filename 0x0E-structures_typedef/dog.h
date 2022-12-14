15e06d149eb6cbf84d15e38cdd6dcb020506cdae
yusuff-fatorisa <bosun4real009@gmail.com>
2022-12-14 04:04:30 -0800

    fix betty error in file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,13 +16,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-/**
- * dog_t - typedef for struct dog
- */
-
-//typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
